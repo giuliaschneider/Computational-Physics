f328f1dd59a656cee19548c9b8d6164a1063c6bd
Giulia <giuliasc@student.ethz.ch>
2018-10-07 08:13:54 +0200

    Refactors plot.py

--- 02-Percolation/python_scripts/plot.py
+++ 02-Percolation/python_scripts/plot.py
@@ -16,9 +16,6 @@ from matplotlib import pyplot as plt
 
 currentDir = os.getcwd()
 resultsDir = abspath(join(currentDir, "../results"))
-files = [f for f in os.listdir(resultsDir) if "3" in f and "100" in f]
-files.sort()
-ylabels = ["Fraction of samples with percolative cluster", "Average shortest path", "Average time of fire"]
 
 
 
@@ -27,33 +24,37 @@ def saveplot(fig, filename):
     fig.savefig(file_str + ".pgf",bbox_inches='tight')
     fig.savefig(file_str + ".png",bbox_inches='tight')
 
-def print_all_plots():
+
+def print_stats():
+
+    files = [f for f in os.listdir(resultsDir) if "3" in f and "100" in f]
+    files.sort()
+    ylabels = ["Fraction of samples with percolative cluster", "Average shortest path", "Average time of fire"]
     p = np.linspace(0,1,101);
 
     for i,filename in enumerate(files):
-
         filepath = join(resultsDir, filename)
+        print(filepath)
         data = np.loadtxt(filepath)
         fig = plt.figure(i,(8.5/2.54,8.5/2.54))
         ax = fig.add_subplot(1,1,1)
-        #ax.plot(data[:1000],data[1:1001],'o')
         ax.plot(p,data)
-
         ax.xaxis.set_ticks_position("bottom")
         ax.yaxis.set_ticks_position("left")
         ax.set_xlim(0,1)
-        #ax.set_ylim(0,31)
         ax.set_xlabel("Occupation probability $p$")
         ax.set_ylabel(ylabels[i])
-        #fig.text(0.15,0.15,r"$p=2^{31}$, $c=16807$", transform=ax.transAxes, bbox=dict(facecolor='white', alpha=0.9))
-        #fig.text(0.15,0.15,r"$p=31$, $c=3$", transform=ax.transAxes, bbox=dict(facecolor='white', alpha=0.9))
-        ax.legend(loc="lower right", frameon=False, labelspacing=0.05)
-        #fig.show()
+
         fname = filename[:-4]
         saveplot(fig, fname)
+        plt.close()
         del data
 
+
 def print_all_spanningClusters():
+    files = [f for f in os.listdir(resultsDir) if "3" in f and "spanningCluster" in f]
+    files.sort()
+
     p = np.linspace(0,1,101);
     legends = ["$N=20$","$N=50$","$N=100$","$N=300$"]
     fig = plt.figure(1,(10/2.54,10/2.54))
@@ -61,20 +62,17 @@ def print_all_spanningClusters():
 
     for i,filename in enumerate(files):
         filepath = join(resultsDir, filename)
+        print(filepath)
         data = np.loadtxt(filepath)
-        #ax.plot(data[:1000],data[1:1001],'o')
         ax.plot(p,data,label=legends[i])
 
         ax.xaxis.set_ticks_position("bottom")
         ax.yaxis.set_ticks_position("left")
         ax.set_xlim(0,1)
-        #ax.set_ylim(0,31)
         ax.set_xlabel("Occupation probability $p$")
         ax.set_ylabel("Fraction of samples with percolative cluster")
-        #fig.text(0.15,0.15,r"$p=2^{31}$, $c=16807$", transform=ax.transAxes, bbox=dict(facecolor='white', alpha=0.9))
-        #fig.text(0.15,0.15,r"$p=31$, $c=3$", transform=ax.transAxes, bbox=dict(facecolor='white', alpha=0.9))
         ax.legend(loc="lower right", frameon=False, labelspacing=0.05)
-        #fig.show()
+
         fname = "3a_all_spanningCluster"
         saveplot(fig, fname)
         del data
@@ -82,5 +80,5 @@ def print_all_spanningClusters():
 
 
 if __name__ == '__main__':
-    print_all_plots()
-    #print_all_spanningClusters()
+    print_stats()
+    print_all_spanningClusters()
