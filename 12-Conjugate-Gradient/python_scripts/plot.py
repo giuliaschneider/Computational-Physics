352906691a69ac68be722b87a1725a0a79732107
giuliaschneider <giuliasc@student.ethz.ch>
2018-12-16 14:51:26 +0100

    Refactors plot.py

--- 12-Conjugate-Gradient/python_scripts/plot.py
+++ 12-Conjugate-Gradient/python_scripts/plot.py
@@ -1,7 +1,7 @@
 import numpy as np
 import sys
 import os as os
-from os.path import isfile, join, dirname, abspath
+from os.path import abspath, basename, dirname, isfile, join
 
 import matplotlib as mpl
 mpl.use("pgf")
@@ -13,77 +13,68 @@ pgf_with_custom_preamble = {
 }
 mpl.rcParams.update(pgf_with_custom_preamble)
 from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import gridspec
-
-def getNumber(s):
-    pos_ = s.find('_')
-    return float(s[:pos_])
 
 
-current_dic = os.getcwd()
-resultsDir = abspath(join(current_dic, "../results"))
-files = [f for f in os.listdir(resultsDir) if "err.txt" in f]
-#files = sorted(files,key=getNumber)
+root = os.getcwd()
+dataDir = abspath(join(root, "..", "results"))
+resultsDir = abspath(join(root, "..", "results", "figures"))
+print(resultsDir)
+
 
 def saveplot(fig, filename):
-    file_str =  join(resultsDir,"figures",filename)
+    file_str =  join(resultsDir, filename)
     fig.savefig(file_str + ".pgf",bbox_inches='tight')
     fig.savefig(file_str + ".png",bbox_inches='tight')
 
 def plot_potential():
-    for i, filename in enumerate(files):
-        filepath = join(resultsDir, filename)
-        data = np.loadtxt(filepath)
+    files = [f for f in os.listdir(dataDir) if ".txt" in f and not "err.txt" in f]
+
+    for i, file in enumerate(files):
+        filename = join(dataDir, file)
+        data = np.loadtxt(filename)
         N = int(np.sqrt(data.size))
         print(N)
 
         data = data.reshape(N,N)
-        #data = data[::-1,:]
         extent = 0,1,1,0
 
         fig = plt.figure(i,(8.5/2.54,8.5/2.54))
         ax = fig.add_subplot(1,1,1)
+        ax.set_axis_off()
         cs = ax.imshow(data,extent=extent)
-        ax.xaxis.tick_top()
-        ax.xaxis.set_ticks_position("top")
-        ax.yaxis.set_ticks_position("left")
-        ax.yaxis.set_ticks(np.arange(0, 1.2, 0.25))
-        #ax.set_xlabel("$x$")
-        #ax.set_ylabel("$z$")
-        #ax.set_xlim(0,11)
-        ax.legend(loc="best", frameon=False, labelspacing=0.05)
-        fig.colorbar(cs)
-        fname = filename[:-4]
+        #ax.xaxis.tick_top()
+        #ax.xaxis.set_ticks_position("top")
+        #ax.yaxis.set_ticks_position("left")
+        #ax.yaxis.set_ticks(np.arange(0, 1.2, 0.25))
+        #fig.colorbar(cs)
+        fname = basename(filename[:-4])
         saveplot(fig, fname)
         plt.close(fig)
 
 
 def plot_convergence():
+    files = [f for f in os.listdir(dataDir) if "err.txt" in f]
+
     fig = plt.figure(1,(8.5/2.54,8.5/2.54))
     ax = fig.add_subplot(1,1,1)
 
-    labels = ["without preconditioning", "with preconditioning"]
+    #labels = ["without preconditioning", "with preconditioning"]
 
-    for i, filename in enumerate(files):
-        print(filename)
-        filepath = join(resultsDir, filename)
-        data = np.loadtxt(filepath)
+    for i, file in enumerate(files):
+        filename = join(dataDir, file)
+        data = np.loadtxt(filename)
         N = data.size
         print(N)
         iteration = np.arange(N)
 
-        ax.loglog(iteration, data, label = labels[i])
+        ax.loglog(iteration, data)
         ax.set_xlabel("$\log(N)$")
         ax.set_ylabel("$\log(r_i^Tr_i)$")
         ax.legend(loc="best", frameon=False, labelspacing=0.05)
 
-    fname = "convergence"
+    fname = basename(filename[:-4])
     saveplot(fig, fname)
     plt.close(fig)
-
-
-
 
 
 if __name__ == '__main__':
