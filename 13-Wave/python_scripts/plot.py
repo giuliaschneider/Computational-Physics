111c30e83cb90938b7e3d63153595f0003e493ee
giuliaschneider <giuliasc@student.ethz.ch>
2019-02-03 10:58:44 +0100

    Refactors plot

--- 13-Wave/python_scripts/plot.py
+++ 13-Wave/python_scripts/plot.py
@@ -1,7 +1,7 @@
 import numpy as np
 import sys
 import os as os
-from os.path import isfile, join, dirname
+from os.path import abspath, basename, dirname, isfile, join
 
 import matplotlib as mpl
 mpl.use("pgf")
@@ -13,8 +13,13 @@ pgf_with_custom_preamble = {
 }
 mpl.rcParams.update(pgf_with_custom_preamble)
 from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import gridspec
+
+
+root = os.getcwd()
+dataDir = abspath(join(root, "..", "results"))
+resultsDir = abspath(join(root, "..", "results", "figures"))
+print(resultsDir)
+
 
 def getNumber(s):
     pos_ = s.find('_t')+2
@@ -22,41 +27,32 @@ def getNumber(s):
     return float(s[pos_:postxt])
 
 
-current_dic = os.getcwd()
-files = [f for f in os.listdir(current_dic) if ".txt" in f]
-files = sorted(files, key=getNumber)
-
-#files = sorted(files,key=getNumber)
-
-a = 0
-b = 20
-
 def saveplot(fig, filename):
-    file_str =  join(dirname(dirname(current_dic)), 'report/figures/') + filename
+    file_str =  join(resultsDir, filename)
     fig.savefig(file_str + ".pgf",bbox_inches='tight')
     fig.savefig(file_str + ".png",bbox_inches='tight')
 
+
+
 def plot_potential():
+    files = [join(dataDir, f )for f in os.listdir(dataDir) if ".txt" in f]
+    files.sort()
+    a = 0
+    b = 20
 
     fig = plt.figure(1,(8.5/2.54,8.5/2.54))
     ax = fig.add_subplot(1,1,1)
 
     for i, filename in enumerate(files):
+        print(filename)
         data = np.loadtxt(filename)
         x = np.linspace(a,b,data.size)
         ax.plot(x,data,label = "$t={}".format(getNumber(filename)))
 
-
-    #ax.xaxis.tick_top()
-    #ax.xaxis.set_ticks_position("top")
-    #ax.yaxis.set_ticks_position("left")
-    #ax.yaxis.set_ticks(np.arange(0, 1.2, 0.25))
     ax.set_xlabel("$x$")
     ax.set_ylabel("$u$")
-    #ax.set_xlim(0,11)
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), frameon=False, labelspacing=0.05)
-    #fig.colorbar(cs)
-    fname = filename[:-4]
+    fname = basename(filename[:-4])
     saveplot(fig, fname)
     plt.close(fig)
 
@@ -65,4 +61,3 @@ def plot_potential():
 
 if __name__ == '__main__':
     plot_potential()
-    print(files)
